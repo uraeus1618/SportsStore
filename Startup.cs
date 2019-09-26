b489cfc53cdb3c15ed8e025eac5fad04cfcd9826
abraxas <alawrence@superpharmacy.com.au>
2019-09-26 16:16:09 +1000

    fuq this shit

--- Startup.cs
+++ Startup.cs
@@ -23,6 +23,7 @@ namespace SportsStore
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+		// how is this
         public void ConfigureServices(IServiceCollection services)
         {
 			services.AddDbContext<DataContext>(options =>
